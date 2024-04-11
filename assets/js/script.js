26743575529804f57dc05abc1d1aaef2f174ab1b
AleDiBernardo <alessandrodibernardo47@gmail.com>
2024-04-11 16:26:13 +0200

    feat: project init and milestone 0 done

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+// Wayne Barnett	Founder & CEO	        wayne-barnett-founder-ceo.jpg
+// Angela Caroll	Chief Editor	        angela-caroll-chief-editor.jpg
+// Walter Gordon	Office Manager	        walter-gordon-office-manager.jpg
+// Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
+// Scott Estrada	Developer	            scott-estrada-developer.jpg
+// Barbara Ramos	Graphic Designer	    barbara-ramos-graphic-designer.jpg
+
+
+const team = [
+    {
+        name: "Wayne Barnett",
+        role: "Founder & CEO",
+        picture: "../img/wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        name: "Angela Caroll",
+        role: "Chief Editor",
+        picture: "../img/angela-caroll-chief-editor.jpg"
+    },
+    {
+        name: "Walter Gordon",
+        role: "Office Manager",
+        picture: "../img/walter-gordon-office-manager.jpg"
+    },
+    {
+        name: "Angela Lopez",
+        role: "Social Media Manager",
+        picture: "../img/angela-lopez-social-media-manager.jpg"
+    },
+    {
+        name: "Scott Estrada",
+        role: "Developer",
+        picture: "../img/scott-estrada-developer.jpg"
+    },
+    {
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+        picture: "../img/barbara-ramos-graphic-designer.jpg"
+    },
+
+]
+
+console.log(team);
